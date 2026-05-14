ee6febbfad54b9a46038ea7e3da7b073b4688320
Ahmed <ahmedzahran851@gmail.com>
2026-05-15 02:08:46 +0300

    Fixed a mistake with the path for astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ahmedzahranali.github.io',
-  base: '/RoadMap_Projects', // Matches your exact GitHub repo name
+  base: '/',
   integrations: [tailwind(), mdx(), sitemap()],
   output: 'static',
 });
